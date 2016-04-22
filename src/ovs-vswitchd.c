3244a9c6362ead42cbfb941f471259193efc866c
Michael Nadler <michael.nadler@hpe.com>
2016-04-22 09:40:25 -0700

    fix taiga 840 - random switchd initialization
    - plugins_init MUST go before bridge_init
    
    Change-Id: I98d6649d68e3e93d847ecac3ebed16ce83daf0d0
    Signed-off-by: Michael Nadler <michael.nadler@hpe.com>

--- src/ovs-vswitchd.c
+++ src/ovs-vswitchd.c
@@ -108,8 +108,13 @@ main(int argc, char *argv[])
     }
     unixctl_command_register("exit", "", 0, 0, ovs_vswitchd_exit, &exiting);
 
-    bridge_init(remote);
+    /*
+     * plugins_init MUST be called PRIOR to bridge_init.  plugins_init loads
+     * all plugins, the calls their init() functions.  These init() functions
+     * may, in turn, want to register for BLK_BR_INIT callback.
+    */
     plugins_init(plugins_path);
+    bridge_init(remote);
 
 #ifdef OPS
     plugins_netdev_register();
