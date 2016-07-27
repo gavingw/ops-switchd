8e530478e0ac068afdd1af0c22f8287a20f0dad1
Sourav Dutta <sourav.dutta@hpe.com>
2016-07-26 17:25:33 -0700

    fix: dev: Adding feature test for subinterface FP
    TG-1136
    
    Change-Id: I58214d64203530acf551141e094c77251c27ac76
    Signed-off-by: Sourav Dutta <sourav.dutta@hpe.com>

--- ops-tests/feature/subinterface/test_layer3_ft_subinterface_route.py
+++ ops-tests/feature/subinterface/test_layer3_ft_subinterface_route.py
@@ -31,22 +31,25 @@ OpenSwitch Tests for subinterface route test using hosts
 from pytest import mark
 
 TOPOLOGY = """
-# +-------+                                 +-------+
-# |       |     +-------+     +-------+     |       |
-# |  hs1  <----->  sw1  <----->  sw2  <----->  hs2  |
-# |       |     +-------+     +-------+     |       |
-# +-------+                                 +-------+
+# +-------+                                 +-------+  +-------+
+# |       |     +-------+     +-------+     |       |  |       |
+# |  hs1  <----->  sw1  <----->  sw2  <----->  hs2  |  |  hs3  |
+# |       |     +-------+     +-------+     |       |  +--------
+# +-------+                       |         +-------+     |
+#                                 |-----------------------|
 
 # Nodes
 [type=openswitch name="Switch 1"] sw1
 [type=openswitch name="Switch 2"] sw2
 [type=host name="Host 1"] hs1
 [type=host name="Host 2"] hs2
+[type=host name="Host 3"] hs3
 
 # Links
 hs1:1 -- sw1:1
 sw1:2 -- sw2:2
 sw2:4 -- hs2:1
+sw2:3 -- hs3:1
 """
 
 
@@ -74,6 +77,14 @@ def check_route(buf, network, nexthop, cli):
     return False
 
 
+def configure_l2_interface(sw, interface, vlan):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.no_shutdown()
+        ctx.no_routing()
+        ctx.vlan_access(vlan)
+
+
+@mark.gate
 @mark.platform_incompatible(['docker'])
 def test_subinterface_route(topology):
     """Test description.
@@ -92,34 +103,43 @@ def test_subinterface_route(topology):
     sw2 = topology.get('sw2')
     hs1 = topology.get('hs1')
     hs2 = topology.get('hs2')
+    hs3 = topology.get('hs3')
 
     assert sw1 is not None
     assert sw2 is not None
     assert hs1 is not None
     assert hs2 is not None
+    assert hs3 is not None
+
+    sw1p1 = sw1.ports["1"]
+    sw1p2 = sw1.ports["2"]
+    sw2p2 = sw2.ports["2"]
+    sw2p3 = sw2.ports["3"]
+    sw2p4 = sw2.ports["4"]
 
     subinterface_vlan = '10'
     sw1_subinterface_ip = '2.2.2.2'
     sw2_subinterface_ip = '2.2.2.1'
     h1_ip_address = '1.1.1.2'
     h2_ip_address = '3.3.3.3'
+    h3_ip_address = '3.3.3.4'
     sw1_l3_ip_address = '1.1.1.1'
     sw2_l3_ip_address = '3.3.3.1'
     mask = '/24'
 
     print("Create subinterface in both switches")
-    configure_subinterface(sw1, '2',
+    configure_subinterface(sw1, sw1p2,
                            sw1_subinterface_ip + mask,
                            subinterface_vlan)
-    configure_subinterface(sw2, '2',
+    configure_subinterface(sw2, sw2p2,
                            sw2_subinterface_ip + mask,
                            subinterface_vlan)
 
     print("Turning on all interfaces used in this test")
-    turn_on_interface(sw1, '1')
-    turn_on_interface(sw1, '2')
-    turn_on_interface(sw2, '2')
-    turn_on_interface(sw2, '4')
+    turn_on_interface(sw1, sw1p1)
+    turn_on_interface(sw1, sw1p2)
+    turn_on_interface(sw2, sw2p2)
+    turn_on_interface(sw2, sw2p4)
 
     print("Configure IP and bring UP in host 1")
     hs1.libs.ip.interface('1', addr=h1_ip_address + mask, up=True)
@@ -134,7 +154,7 @@ def test_subinterface_route(topology):
     hs2.libs.ip.add_route('1.1.1.0/24', '3.3.3.1')
 
     print("Configure L3 interface IP address on switch 1")
-    with sw1.libs.vtysh.ConfigInterface('1') as ctx:
+    with sw1.libs.vtysh.ConfigInterface(sw1p1) as ctx:
         ctx.ip_address(sw1_l3_ip_address + mask)
         ctx.no_shutdown()
 
@@ -143,7 +163,7 @@ def test_subinterface_route(topology):
         ctx.ip_route('3.3.3.0/24', '2.2.2.1')
 
     print("Configure IP address on switch 2")
-    with sw2.libs.vtysh.ConfigInterface('4') as ctx:
+    with sw2.libs.vtysh.ConfigInterface(sw2p4) as ctx:
         ctx.ip_address(sw2_l3_ip_address + mask)
         ctx.no_shutdown()
 
@@ -183,11 +203,25 @@ def test_subinterface_route(topology):
     if pass_flag == 0:
         sw1.libs.vtysh.show_running_config()
         sw1.libs.vtysh.show_ip_route()
-        sw1.libs.vtysh.show_interface('1')
-        sw1.libs.vtysh.show_interface('2')
+        sw1.libs.vtysh.show_interface(sw1p1)
+        sw1.libs.vtysh.show_interface(sw1p2)
         sw2.libs.vtysh.show_running_config()
         sw2.libs.vtysh.show_ip_route()
-        sw2.libs.vtysh.show_interface('2')
-        sw2.libs.vtysh.show_interface('4')
+        sw2.libs.vtysh.show_interface(sw2p2)
+        sw2.libs.vtysh.show_interface(sw2p4)
     assert ping['transmitted'] == ping['received'],\
         'Ping between ' + h1_ip_address + ' and ' + h2_ip_address + ' failed'
+
+    print("Configure IP and bring UP in host 3")
+    hs3.libs.ip.interface('1', addr=h3_ip_address + mask, up=True)
+
+    print("Configure Vlan 10")
+    with sw2.libs.vtysh.ConfigVlan(subinterface_vlan) as ctx:
+        ctx.no_shutdown()
+
+    print("Configure l2 interface with vlan " + subinterface_vlan)
+    configure_l2_interface(sw2, sw2p3, subinterface_vlan)
+    print("Ping h2 to host 3")
+    ping = hs2.libs.ping.ping(10, h3_ip_address)
+    assert ping['received'] == 0,\
+        'Ping between ' + h1_ip_address + ' and ' + h2_ip_address + ' passed'
